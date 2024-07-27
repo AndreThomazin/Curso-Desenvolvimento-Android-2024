3f8171ba8baf851c1b0cac6b8363c353a8203970
AndreThomazin <99302845+AndreThomazin@users.noreply.github.com>
2024-07-27 12:24:32 -0400

    Implementação da camada Controller - MVC no método do botão salvar

--- AppListaCurso/app/src/main/java/devandroid/thomazin/applistacurso/controller/PessoaController.java
+++ AppListaCurso/app/src/main/java/devandroid/thomazin/applistacurso/controller/PessoaController.java
@@ -1,4 +1,23 @@
 package devandroid.thomazin.applistacurso.controller;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import devandroid.thomazin.applistacurso.model.Pessoa;
+
 public class PessoaController {
+
+    @NonNull
+    @Override
+    public String toString() {
+
+        Log.d("MVC_Controller", "Controller iniciada...");
+
+        return super.toString();
+    }
+
+    public void salvar(Pessoa pessoa) {
+        Log.i("MVC_Controller", "Salvo: "+pessoa.toString());
+    }
 }
